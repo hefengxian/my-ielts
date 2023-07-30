b769d54e3179ad8bcfaf13fee6d8ba69b9dca4cf
Frank <jssmith883@gmail.com>
2023-07-31 00:58:31 +0800

    Change page node structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <!-- <main>
-    <TheHeader />
-    <RouterView />
-    <TheFooter />
-  </main> -->
-
   <TheHeader />
-  <div class="flex overflow-hidden bg-gray-50 pt-16 dark:bg-gray-900">
-    <div id="main-content" class="relative mx-auto h-full w-full max-w-screen-2xl overflow-y-auto bg-gray-50 dark:bg-gray-900">
-      <main>
-        <RouterView />
-      </main>
-      <TheFooter />
+  <!-- <div class="flex flex-1 overflow-hidden bg-gray-50 pt-16 dark:bg-gray-900">
+    <div
+      class="relative mx-auto h-full w-full max-w-screen-2xl overflow-y-auto bg-gray-50 dark:bg-gray-900 dark:text-white"
+    />
+  </div> -->
+  <div class="flex flex-1 overflow-hidden bg-gray-50 py-16 dark:bg-gray-900">
+    <div class="relative mx-auto h-full w-full max-w-screen-2xl overflow-y-auto bg-gray-50 dark:bg-gray-900 dark:text-white">
+      <RouterView />
     </div>
   </div>
+
+  <TheFooter />
 </template>
