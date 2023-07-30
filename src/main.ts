e5449b34fbc9f7d9d7e8e4a489fb1bc94fbea8f4
Frank <jssmith883@gmail.com>
2023-07-31 00:57:33 +0800

    Change router to hash model

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
@@ -9,7 +9,8 @@ import 'uno.css'
 
 const app = createApp(App)
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // 改成 Hash 模式
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 app.use(router)
