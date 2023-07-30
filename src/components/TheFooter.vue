be3942369de63dbc9839d1c7b6f7a02110965597
Frank <jssmith883@gmail.com>
2023-07-31 00:55:54 +0800

    Remove footer's unrelated description

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,14 +4,14 @@ const year = (new Date()).getFullYear()
 
 <template>
   <footer class="px-4 py-6 md:flex md:items-center md:justify-between 2xl:px-0 md:py-10">
-    <p class="mb-4 text-center text-sm text-gray-500 md:mb-0">
+    <p class="mb-4 flex-1 text-center text-sm text-gray-500 md:mb-0">
       &copy; {{ year }} <a href="/" class="hover:underline" target="_blank">Frank</a>. All rights reserved.
     </p>
-    <ul class="flex flex-wrap items-center justify-center">
+    <!-- <ul class="flex flex-wrap items-center justify-center">
       <li><a href="#" class="mr-4 text-sm font-normal text-gray-500 md:mr-6 dark:text-gray-400 hover:underline">Terms</a></li>
       <li><a href="#" class="mr-4 text-sm font-normal text-gray-500 md:mr-6 dark:text-gray-400 hover:underline">Licensing</a></li>
       <li><a href="#" class="mr-4 text-sm font-normal text-gray-500 md:mr-6 dark:text-gray-400 hover:underline">Cookie Policy</a></li>
       <li><a href="#" class="text-sm font-normal text-gray-500 dark:text-gray-400 hover:underline">Contact</a></li>
-    </ul>
+    </ul> -->
   </footer>
 </template>
