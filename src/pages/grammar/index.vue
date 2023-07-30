fc9fe9c4280b5101f15ae6be727503fabcd3247b
Frank <jssmith883@gmail.com>
2023-07-31 00:58:02 +0800

    Add youtube embed video

--- src/pages/grammar/index.vue
+++ src/pages/grammar/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="px-4 pt-6 2xl:px-0">
     <div class="mb-4 border border-gray-200 rounded-lg bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/M82MfdqI6zg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen />
       <ul>
         <li>
           <a href="https://www.youtube.com/playlist?list=PLK6q4vrvWZiM4-U3SSs5pQXf2E7WMQ4H2"><i class="i-carbon-link inline-block" />《雅思英语基础语法》</a>
