19fe9c769a717d206b62260d913237d632ba7433
Frank <jssmith883@gmail.com>
2023-07-31 01:19:12 +0800

    Migrate IELTS vocabulary to this project

--- src/pages/vocabulary/index.vue
+++ src/pages/vocabulary/index.vue
@@ -1,9 +1,254 @@
+<!-- eslint-disable eslint-comments/no-unlimited-disable -->
+<script setup generic="T extends any, O extends any">
+import { computed } from 'vue'
+import vocabulary from './vocabulary'
+
+const keyword = ref('')
+const category = ref('自然地理')
+const loaded = ref(false)
+const refVocabulary = reactive(vocabulary)
+const wordList = computed(() => {
+  const result = structuredClone(vocabulary) // deep clone
+  const keywordValue = keyword.value.trim().toLowerCase()
+  const categoryValue = category.value
+
+  if (categoryValue !== '') {
+    for (const key in result) {
+      if (key !== categoryValue)
+        delete result[key]
+    }
+  }
+
+  if (keywordValue !== '') {
+    for (const key in result) {
+      const category = result[key]
+      const words = []
+      category.words.forEach((group) => {
+        words.push(group.filter((item) => {
+          return item.word.toLowerCase().includes(keywordValue)
+        }))
+      })
+      category.words = words
+    }
+  }
+  return result
+})
+
+onMounted(() => {
+  loaded.value = true
+
+  // 只能同时播放一个音频
+  const audioTags = document.getElementsByTagName('audio')
+  for (const audio of audioTags) {
+    audio.onplay = () => {
+      for (const _audio of audioTags) {
+        _audio.blur()
+        if (audio !== _audio)
+          _audio.pause()
+      }
+    }
+  }
+})
+
+onUpdated(() => {
+  // 音频再切换 SRC 之后需要调用一下 load() 不然看不到效果
+  for (const el of document.getElementsByTagName('audio'))
+    el.load()
+})
+
+document.addEventListener('keyup', (ev) => {
+  // 激活的那个音频可以通过方向键进行快进/退
+  if (['ArrowLeft', 'ArrowRight'].includes(ev.key)) {
+    ev.preventDefault()
+    const audioTags = document.getElementsByTagName('audio')
+    for (const audioTag of audioTags) {
+      if (!audioTag.paused) {
+        audioTag.blur()
+        const step = (ev.key === 'ArrowLeft' ? -3 : 3)
+        audioTag.currentTime = audioTag.currentTime + step
+        // console.log(step, audioTag.currentTime)
+      }
+    }
+  }
+})
+</script>
+
 <template>
   <div class="px-4 pt-6 2xl:px-0">
-    <div class="mb-4 border border-gray-200 rounded-lg bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
-      <ul>
-        <li>雅思词汇真经 TODO</li>
-      </ul>
+    <div class="border border-gray-200 rounded-lg bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
+      <!-- Card header -->
+      <div class="items-center justify-between lg:flex">
+        <div class="mb-4 lg:mb-0">
+          <h3 class="mb-2 text-xl font-bold text-gray-900 dark:text-white">
+            雅思词汇真经
+          </h3>
+          <span class="text-base font-normal text-gray-500 dark:text-gray-400">涵盖雅思必备核心词，逻辑词群记忆法</span>
+        </div>
+        <div class="items-center sm:flex">
+          <div class="flex items-center">
+            <select
+              v-model="category"
+              class="block w-full flex-1 border border-gray-300 rounded-lg bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 focus:border-blue-500 dark:bg-gray-700 dark:text-white focus:ring-blue-500 dark:focus:border-blue-500 dark:focus:ring-blue-500 dark:placeholder-gray-400"
+            >
+              <option value="">
+                全部章节
+              </option>
+              <option
+                v-for="(_, k) in refVocabulary"
+                :key="k"
+                :value="k"
+              >
+                {{ k }}
+              </option>
+            </select>
+            <!-- <input type="text" name="email" class="ml-3 block w-full border border-gray-300 rounded-lg bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 focus:border-primary-500 dark:bg-gray-700 sm:text-sm dark:text-white focus:ring-primary-500 dark:focus:border-primary-500 dark:focus:ring-primary-500 dark:placeholder-gray-400" placeholder="关键词"> -->
+            <div class="relative ml-2 flex-1">
+              <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+              </div>
+              <input
+                v-model="keyword"
+                type="search"
+                class="block w-full border border-gray-300 rounded-lg bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 dark:border-gray-600 focus:border-blue-500 dark:bg-gray-700 dark:text-white focus:ring-blue-500 dark:focus:border-blue-500 dark:focus:ring-blue-500 dark:placeholder-gray-400" placeholder="Search"
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Table -->
+      <div class="mt-6 flex flex-col">
+        <div class="overflow-x-auto rounded-lg">
+          <div class="inline-block min-w-full align-middle">
+            <div class="overflow-hidden shadow sm:rounded-lg">
+              <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-600">
+                <thead class="bg-gray-50 dark:bg-gray-700">
+                  <tr>
+                    <th class="p-4 text-left text-xs font-medium tracking-wider uppercase text-gray-500 dark:text-white">
+                      #
+                    </th>
+                    <th class="p-4 text-left text-xs font-medium tracking-wider uppercase text-gray-500 dark:text-white">
+                      词
+                    </th>
+                    <th class="w-0 text-gray-500 dark:text-white">
+                      词性
+                    </th>
+                    <th class="p-4 text-left text-xs font-medium tracking-wider uppercase text-gray-500 dark:text-white">
+                      词义
+                    </th>
+                    <th class="p-4 text-left text-xs font-medium tracking-wider uppercase text-gray-500 dark:text-white">
+                      例句
+                    </th>
+                    <th class="p-4 text-left text-xs font-medium tracking-wider uppercase text-gray-500 dark:text-white">
+                      拓展
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white dark:bg-gray-800">
+                  <template
+                    v-for="(_category, categoryLabel) in wordList"
+                    :key="categoryLabel"
+                  >
+                    <tr class="bg-hex-f3f3f3">
+                      <td colspan="6" class="px-4 py-6 text-sm font-normal text-gray-900 dark:bg-gray-500 dark:text-white">
+                        <div class="flex flex-row">
+                          <div class="flex flex-1 items-center">
+                            <span class="text-lg">{{ categoryLabel }}</span>
+                            （ {{ _category.groupCount }} 组 {{ _category.wordCount }} 个词 ）
+                          </div>
+                          <div class="justify-items-end">
+                            <audio controls>
+                              <source :src="`vocabulary/audio/${_category.audio}`" type="audio/mpeg">
+                            </audio>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <template
+                      v-for="(wordGroup, i) of _category.words"
+                      :key="wordGroup.label"
+                    >
+                      <tr
+                        v-for="item of wordGroup"
+                        :key="item.id"
+                        :class="{ 'bg-gray-50 dark:bg-gray-700': item.id % 2 === 0, [`group-color-${i % 15}`]: true }"
+                        class="text-sm text-gray-900 dark:text-white"
+                      >
+                        <td class="p-4">
+                          {{ item.id }}
+                        </td>
+                        <td class="whitespace-nowrap p-4">
+                          <a
+                            class="hover:underline"
+                            :title="`在剑桥词典中查询 ${item.word}`"
+                            target="_blank"
+                            :href="`https://dictionary.cambridge.org/dictionary/english-chinese-simplified/${item.word}`"
+                          >{{ item.word }}</a>
+                        </td>
+                        <td style="font-style: italic; font-family: times;">
+                          {{ item.pos }}
+                        </td>
+                        <td class="p-4">
+                          {{ item.meaning }}
+                        </td>
+                        <td class="p-4">
+                          {{ item.example }}
+                        </td>
+                        <td class="p-4">
+                          {{ item.extra }}
+                        </td>
+                      </tr>
+                    </template>
+                  </template>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Card Footer -->
+      <div class="flex items-center justify-between pt-3 sm:pt-6">
+        <div>
+          <button class="inline-flex items-center rounded-lg p-2 text-center text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" type="button" data-dropdown-toggle="transactions-dropdown">
+            Last 7 days <svg class="ml-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" /></svg>
+          </button>
+          <!-- Dropdown menu -->
+          <div id="transactions-dropdown" class="z-50 my-4 hidden list-none rounded bg-white text-base shadow divide-y divide-gray-100 dark:bg-gray-700 dark:divide-gray-600">
+            <div class="px-4 py-3" role="none">
+              <p class="truncate text-sm font-medium text-gray-900 dark:text-white" role="none">
+                Sep 16, 2021 - Sep 22, 2021
+              </p>
+            </div>
+            <ul class="py-1" role="none">
+              <li>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Yesterday</a>
+              </li>
+              <li>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Today</a>
+              </li>
+              <li>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Last 7 days</a>
+              </li>
+              <li>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Last 30 days</a>
+              </li>
+              <li>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Last 90 days</a>
+              </li>
+            </ul>
+            <div class="py-1" role="none">
+              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Custom...</a>
+            </div>
+          </div>
+        </div>
+        <div class="flex-shrink-0">
+          <a href="#" class="inline-flex items-center rounded-lg p-2 text-xs font-medium uppercase text-primary-700 hover:bg-gray-100 sm:text-sm dark:text-primary-500 dark:hover:bg-gray-700">
+            Transactions Report
+            <svg class="ml-1 h-4 w-4 sm:h-5 sm:w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" /></svg>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
---
+++ src/pages/vocabulary/parser.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+from collections import defaultdict
+from urllib import request
+
+CUR_DIR = Path(__file__).absolute().parent
+
+
+def parse():
+    part_mapping = {
+        0: 'word',
+        1: 'pos',
+        2: 'meaning',
+        3: 'example',
+        4: 'extra',
+    }
+    result = defaultdict(lambda: {'label': '', 'audio': '', 'groupCount': 0, 'wordCount': 0, 'words': []})
+    vocabulary_path = CUR_DIR / 'vocabulary.txt'
+    contents = vocabulary_path.read_text()
+    categories = contents.split('===\n')
+    cur_id = 0
+    category_index = 0
+    for category in categories:
+        category_index += 1
+        category_parts = category.split('+++\n')
+        label = category_parts[0].strip()
+        category_body = result[label]
+        word_groups = category_parts[1].split('---\n')
+        category_body['label'] = label
+        category_body['audio'] = f"{str(category_index).zfill(2)}_{label}.mp3"
+        category_body['groupCount'] = len(word_groups)
+        word_count = 0
+        for word_group in word_groups:
+            words = word_group.strip().split('\n')
+            group = []
+            for word in words:
+                word_count += 1
+                cur_id += 1
+                word_parts = word.split('|')
+                word_dict = {'id': cur_id}
+                for part_index in part_mapping:
+                    dict_key = part_mapping[part_index]
+                    word_dict[dict_key] = word_parts[part_index] if part_index < len(
+                        word_parts) else '-'
+                group.append(word_dict)
+            category_body['words'].append(group)
+        category_body['wordCount'] = word_count
+
+    js_code = f"""
+/**
+  * pos = part of speech
+  */
+
+const vocabulary = {dict(result).__str__()}
+
+export default vocabulary
+"""
+    vocabulary_js_file = CUR_DIR / 'vocabulary.js'
+    vocabulary_js_file.write_text(js_code)
+
+
+def download_audio():
+    names = ['01_自然地理', '02_植物研究', '03_动物保护', '04_太空探索', '05_学校教育', '06_科技发明', '07_文化历史', '08_语言演化', '09_娱乐运动', '10_物品材料', '11_时尚潮流',
+             '12_饮食健康', '13_建筑场所', '14_交通旅行', '15_国家政府', '16_社会经济', '17_法律法规', '18_沙场争锋', '19_社会角色', '20_行为动作', '21_身心健康', '22_时间日期']
+
+    for index in range(22):
+        url = f"https://down.guixue.com/mp3/ielts_lexicon/{index + 1}.mp3"
+        file_name, headers = request.urlretrieve(url)
+        Path(file_name).rename(CUR_DIR / f'audio/{names[index]}.mp3')
+
+
+if __name__ == '__main__':
+    # download_audio()
+    parse()
+    pass
