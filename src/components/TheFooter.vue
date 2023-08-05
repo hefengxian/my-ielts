94124d0fa83e87724afdbb23f5416ec8bba423f5
Frank <jssmith883@gmail.com>
2023-08-05 20:34:41 +0800

    Fix the dark model of footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,7 +3,7 @@ const year = (new Date()).getFullYear()
 </script>
 
 <template>
-  <footer class="px-4 py-6 md:flex md:items-center md:justify-between 2xl:px-0 md:py-10">
+  <footer class="px-4 py-6 md:flex md:items-center md:justify-between dark:bg-gray-800 2xl:px-0 md:py-10">
     <p class="mb-4 flex-1 text-center text-sm text-gray-500 md:mb-0">
       &copy; {{ year }} <a href="/" class="hover:underline" target="_blank">Frank</a>. All rights reserved.
     </p>
